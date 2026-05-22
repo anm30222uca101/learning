e80528067c574e71c271b3aad6c29992df8c9459
Tamilarasan <tamilarasan2004gbhss@gmail.com>
2026-05-22 18:14:40 +0530

    i exicute the do while

---
+++ looping/dowhile.java
@@ -0,0 +1,13 @@
+class dowhile {
+
+    public static void main(String[] args) {
+
+        char ch = 'A';
+
+        do {
+            System.out.println(ch);
+            ch++;
+        } while(ch <= 'E');
+
+    }
+}
